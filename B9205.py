aff65203017a3269357e47637876b8ce36dff46d
DESKTOP-64VHHIQ\rha45 <rha4578@gmail.com>
2021-06-22 14:37:43 +0900

    2021/6/22 pratice

--- B9205.py
+++ B9205.py
@@ -3,7 +3,17 @@ from collections import deque
 T=int(input())
 
 for t in range(T):
+    answer=""
     N=int(input()) #편의점의 개수
-    field=[[0 for _ in range(N)] for _ in range(N)]
+    field=[[0 for _ in range(2)] for _ in range(N)]
     x1, y1=map(int, input().split())
+    for n in range(N):
+        field[i][0]=int(input())
+        field[i][1]=int(input())
     x2, y2=map(int, input().split())
+
+    visit=[[0 for _ in range(2)] for _ in range(N+2)]
+    queue=deque()
+    while queue:
+
+    print(answer)
